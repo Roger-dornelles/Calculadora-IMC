3880f2addeb38d3a35cfdef4219f3802dbb98d73
Roger dornelles <rsd.santo@outlook.com>
2022-02-01 15:53:56 -0300

    Atualização na função da calculadora e cor do botão para calcular IMC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>{
+  const [peso, setPeso ] = useState('');
+  const [ altura, setAltura ] = useState('');
+  const [info, setInfo ] = useState('');
+
+  const calcularImc = ()=>{
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if(peso || altura === ''){
+      setInfo('Preencha todos os campos...')
+    }
+
+    if(imc < 18.6){
+      setInfo(`Você esta abaixo do peso,seu IMC: ${imc.toFixed(2)}`);
+    }else if(imc >= 18.6 && imc <24.9){
+      setInfo(`Peso ideal,seu IMC: ${imc.toFixed(2)}`);
+    }else if(imc >= 24.9 && imc < 34.9){
+      setInfo(`Você esta levemente acima do peso,seu IMC: ${imc.toFixed(2)}`);
+    }else if(imc > 34.9){
+      setInfo(`Cuidado OBESIDADE,seu IMC: ${imc.toFixed(2)}`);
+    }
+  }
+  return(
+    <div className="app">
+
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu IMC.</span>
+
+      <div className="inputs">
+        <input type="text"
+          placeholder="Peso em (KG) Ex:90"
+          value={peso}
+          onChange={e => setPeso(e.target.value)}
+        />
+        <input type="text"
+          placeholder="Altura em (CM) EX:180"
+          value={altura}
+          onChange={e => setAltura(e.target.value)}
+          />
+
+        <button onClick={calcularImc}>Calcular</button>
+      </div>
+      <h2>{info}</h2>
     </div>
-  );
+  )
 }
 
 export default App;
